7dfdb5c253ef340faa2773bec8b9fca317028fd6
mkcoder1218 <codemk1218@gmail.com>
2025-10-05 06:15:39 +0300

    chore: update api client base url to new server address

--- axios/api.ts
+++ axios/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: 'https://shaft-usd-respiratory-narrow.trycloudflare.com/api', // dynamic base URL
+  baseURL: 'http://91.98.46.158:3002/api', // dynamic base URL
   headers: {
     'Content-Type': 'application/json',
   },
